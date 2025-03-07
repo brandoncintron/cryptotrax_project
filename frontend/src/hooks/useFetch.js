60046f1c9841ea9c4724db2c8ab0553cb814efca
brandoncintron <152916352+brandoncintron@users.noreply.github.com>
2025-03-06 20:12:10 -0500

    Added additional comments

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react';
 
+// Main data fetching function
 const useFetch = (endpoint, id, cacheKey, cacheDuration = 180000) => {
+  // Use state for data, loading status, and errors to send to components
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   // Utilize session storage to save data for 3 mins
   useEffect(() => {
+    // Check session storage for already cached data
     const cached = sessionStorage.getItem(cacheKey);
     if (cached) {
       const parsedCache = JSON.parse(cached);
@@ -14,16 +17,16 @@ const useFetch = (endpoint, id, cacheKey, cacheDuration = 180000) => {
       if (new Date().getTime() - parsedCache.timestamp < cacheDuration) {
         setData(parsedCache.data);
         setLoading(false);
-        //console.log("Cache is valid."); // debug
         return;
-      } //console.log("Cache not valid, re-calling api...") // debug
+      }
     }
-
+    // Fetch data from API and update state and cache
     const fetchData = async () => {
       try {
         const url = `${import.meta.env.VITE_BACKEND_URL}${endpoint.replace(':id', id)}`;
         const response = await fetch(url);
         if (!response.ok) throw new Error('Failed to fetch');
+        // Update state with fresh data
         const result = await response.json();
         setData(result);
         // Set the fetched data into session storage with current time
@@ -34,6 +37,7 @@ const useFetch = (endpoint, id, cacheKey, cacheDuration = 180000) => {
       } catch (err) {
         setError('Failed to load data.');
       } finally {
+        // Turn off loading when done
         setLoading(false);
       }
     };
@@ -41,6 +45,7 @@ const useFetch = (endpoint, id, cacheKey, cacheDuration = 180000) => {
     fetchData();
   }, [endpoint, id, cacheKey, cacheDuration]);
 
+  // Return state values for components
   return { data, loading, error };
 };
 
